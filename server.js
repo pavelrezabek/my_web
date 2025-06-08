083f510c7baa4815c0c8f6ebe62a6a5bcc336847
Pavel Řežábek <pavelrezabek@Pavels-MacBook-Air.local>
2025-06-08 18:59:56 +0200

    change of the routing of css

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const PORT = 3000;
 // Slouží statické soubory z adresáře "public"
 app.use(express.static('public'));
 
+
+// routování pro statické soubory
+app.get('/style.css', (req, res) => {
+  res.sendFile(__dirname + '/public/style.css');
+});
+app.get('/compiled/script.js', (req, res) => {
+  res.sendFile(__dirname + '/public/compiled/script.js');
+});
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
